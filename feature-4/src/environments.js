08cfcfaafe3489a355c760fbab229b6befbe924a
Carolina Carpenetti <122832874+ccarpene@users.noreply.github.com>
2025-04-02 23:33:06 -0400

    Delete feature-4 directory

--- feature-4/src/environments.js
+++
@@ -1,7 +0,0 @@
-export default function (){
-    return {
-    APPLICATION_ID: "sbz3OZBLJK2o8LACBZZ9yYPGeXHV4rHhugAitS4T",
-    JAVASCRIPT_KEY: "Rru2V5vsS7KVgUlroYEU8WpvN2ZBrlD7tbrJLhwb",
-    SERVER_URL: "https://parseapi.back4app.com/"
-    }
-}
